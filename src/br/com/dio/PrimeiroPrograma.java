a56aab5e0853b143c692dd0ac5d8cd8c27ccaa3d
Diogo Sousa <sousadiogo453@gmail.com>
2023-03-10 23:21:52 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,9 +8,39 @@ public class PrimeiroPrograma {
 
         Gato gato = new Gato();
 
+        Livro livro = new Livro("O Problmea dos 3 corpos", 300);
+        System.out.println(livro);
+
        /* int a = 5;
         int b = 3;
 
         System.out.println("Olá Mundo " + (a+b));*/
     }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer NumPaginas){
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
 }
